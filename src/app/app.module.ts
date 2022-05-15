95772d0d9d613a275bfa3240f6adc0f3d38a05c7
Edson Silva <edson@purespectrum.com>
2022-05-15 17:43:53 -0300

    feature/PSA-1 - reactive forms module installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 // Bootstrap stuff
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
